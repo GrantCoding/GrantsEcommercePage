4fdee91785c060bcf56cf4e1ec2c82c7756de067
Grant <76258417+GrantCoding@users.noreply.github.com>
2023-09-12 16:03:16 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/GrantCoding/ecommerce-page/controllers"
+	"github.com/GrantCoding/ecommerce-page/database"
+	"github.com/GrantCoding/ecommerce-page/middleware"
+	"github.com/GrantCoding/ecommerce-page/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	port := os.Getenv("PORT")
+	if port == ""{
+		port = "8000"
+	}
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+	
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", controllers.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+	log.Fatal(router.Run(":" + port))
+
+
+}
